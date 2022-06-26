05fe13c8eb950776a1d23a2c24881dd241e6176d
Minwoo Son <mwson98@naver.com>
2022-06-27 00:29:23 +0900

    Update Open1.java
    0 to 1

--- app/src/main/java/com/example/temisdk/Open1.java
+++ app/src/main/java/com/example/temisdk/Open1.java
@@ -9,12 +9,20 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Open1 extends AppCompatActivity {
 
     TextView textState;
     Button buttonBack;
 
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("room1");    
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +33,7 @@ public class Open1 extends AppCompatActivity {
         buttonBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                myRef.setValue(1);
                 Intent intent = new Intent(getApplicationContext(),PressActivity.class);
                 startActivity(intent);
             }
